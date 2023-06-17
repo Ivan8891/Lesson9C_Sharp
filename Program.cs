f721765f1d29e84ee95807fdc847318fa25095c0
Ivan <crueldog50@gmail.com>
2023-06-17 17:43:08 +1000

    task 66

--- Program.cs
+++ Program.cs
@@ -19,4 +19,19 @@ string PrintNumber(int n)
     return n + " " + PrintNumber(n - 1);
 }
 
-Console.WriteLine(PrintNumber(EnterNumber("Введите число для старта: ")));
+//Console.WriteLine(PrintNumber(EnterNumber("Введите число для старта: ")));
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int PrintSum(int m, int n)
+{
+    if(m == n)
+    {
+        return m;
+    }
+    return m + PrintSum(m + 1, n);
+}
+
+Console.WriteLine(PrintSum(EnterNumber("Введите число M: "), EnterNumber("Введите число N: ")));
